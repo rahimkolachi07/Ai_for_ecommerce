e92a775fc2300eb27d1363ee476e178f8d97731f
rahimkolachi07 <raheemkolachi7@gmail.com>
2024-03-08 14:57:58 +0500

    updated code

--- app.py
+++ app.py
@@ -5,17 +5,26 @@ from main import product_exp, translated
 import time
 
 def process_csv(product, translate):
-    df1 = pd.read_csv(product.name)
-    df1=product_exp(df1)
-    time.sleep(2)
-    df2 = pd.read_csv(translate.name)
-    df2=translated(df2)
-    # Save the dataframes as CSV files with the same names as the input files
-    df1.to_csv(product.name, index=False)
-    df2.to_csv(translate.name, index=False)
-    
-    # Return the paths of the CSV files
-    return product.name, translate.name
+    a=1
+    while a==1:
+        try:
+            df1 = pd.read_csv(product.name)
+            df1=product_exp(df1)
+            time.sleep(2)
+            df2 = pd.read_csv(translate.name)
+            df2=translated(df2)
+            # Save the dataframes as CSV files with the same names as the input files
+            df1.to_csv(product.name, index=False)
+            df2.to_csv(translate.name, index=False)
+            a=0
+            return product.name, translate.name
+        except:
+            df1 = pd.read_csv(product.name)
+            df2 = pd.read_csv(translate.name)
+            df1.to_csv(product.name, index=False)
+            df2.to_csv(translate.name, index=False)
+            return product.name, translate.name
+
 
 iface = gr.Interface(
     fn=process_csv, 
