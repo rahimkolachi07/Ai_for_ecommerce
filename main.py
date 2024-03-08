f85c22d77384d764d8b63c1eac4a4c048a37a1d9
rahimkolachi07 <raheemkolachi7@gmail.com>
2024-03-08 15:15:55 +0500

    update

--- main.py
+++ main.py
@@ -5,23 +5,34 @@ import time
 
 def product_exp(data):
     for i, b in enumerate(data["Body (HTML)"]):
-        if "str" in str(type(b)):
-            text = g_model(f"Translate  into Netherlands. {data.loc[i, 'Body (HTML)']}")
-            print(text)
-            data.loc[i, 'Body (HTML)'] = text
-    return data
+        z=1
+        while z==1:
+            try:
+                if "str" in str(type(b)):
+                    text = g_model(f"Translate  into Netherlands. {data.loc[i, 'Body (HTML)']}")
+                    print(text)
+                    data.loc[i, 'Body (HTML)'] = text
+                    z=0
+                    return data
+            except:
+                z=1
+            
 
 def translated(data):
     # Ensure the 'Translated content' column is of type object
     data['Translated content'] = data['Translated content'].astype(object)
     
     for i, b in enumerate(data["Locale"]):
-        try:
-            if "str" in str(type(b)):
-                text = g_model(f"translate this text = {data['Default content'][i]} into = {b} language")
-                print(text)
-                data.loc[i, 'Translated content'] = text
-        except:
-            print("error")
-            pass
+        z=1
+        while z==1:
+            try:
+                if "str" in str(type(b)):
+                    text = g_model(f"translate this text = {data['Default content'][i]} into = {b} language")
+                    print(text)
+                    data.loc[i, 'Translated content'] = text
+                    z=0
+            except:
+                z=1
+                print("error")
+                pass
     return data
